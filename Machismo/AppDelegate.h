de23d424136d77517669076b4bca9ba523383d68
carlos calderon <carlos@carloss-macbook-pro.local>
2016-01-25 23:17:44 -0600

    Initial Commit

---
+++ Machismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Machismo
+//
+//  Created by carlos calderon on 1/25/16.
+//  Copyright © 2016 carlos calderon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
